f28726d79e09d235bbc53e6c7b50a304a1359ad9
bangjaehun <qkdwogns98@gmail.com>
2021-03-30 23:05:16 +0900

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+// @see https://stackoverflow.com/questions/37693982/how-to-fetch-xml-with-fetch-api
+// @see https://www.npmjs.com/package/xml-js
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Cases from "../components/Cases";
@@ -6,14 +12,8 @@ import RegionalTable from "../components/RegionalTable";
 
 import HomeStyles from "../styles/Home.module.css";
 
-import { fetchData } from "./api/fetchData";
-
-type HomeProps = {
-  covidData: {};
-};
-
 const Home = ({ data }) => {
-  console.log(data);
+  console.log(data.response.body.items);
 
   return (
     <div className={HomeStyles.container}>
@@ -27,7 +27,9 @@ const Home = ({ data }) => {
 };
 
 Home.getInitialProps = async () => {
-  const data = await fetchData();
+  const { data } = await axios.get(
+    "http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey=fUio6BUTWluJVfLQpEDGp5Goep1YvqAVJt2%2Fz2WOoFbsyaJYNQ0shUPRlgryta5ytgbONMa2B8lmozCwOGAJwA%3D%3D&pageNo=1&numOfRows=10&startCreateDt=20200310&endCreateDt=20200315"
+  );
 
   return {
     data,
