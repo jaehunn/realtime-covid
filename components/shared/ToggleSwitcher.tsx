0752d1463725444e914d1b9bf297f28ccc0b7ba9
bangjaehun <qkdwogns98@gmail.com>
2021-05-23 21:15:39 +0900

    ...

--- components/shared/ToggleSwitcher.tsx
+++ components/shared/ToggleSwitcher.tsx
@@ -6,7 +6,7 @@ const ToggleSwitcher = () => {
 
   return (
     <button className="w-20 h-10 mt-10 rounded-full bg-gray-200 flex items-center transition duration-300 focus:outline-none" onClick={themeToggle}>
-      <div className="w-12 h-10 relative rounded-full transition duration-500 transform p-1 text-white leading-10" ref={toggleEl}></div>
+      <div className="w-12 h-10 relative rounded-full transition duration-500 transform p-1 leading-10" ref={toggleEl}></div>
     </button>
   );
 };
